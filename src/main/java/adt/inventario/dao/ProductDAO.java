519eb38b1d5539445e791e712c1121f49b0ec397
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 21:04:33 +0100

    Add crud

--- src/main/java/adt/inventario/dao/ProductDAO.java
+++ src/main/java/adt/inventario/dao/ProductDAO.java
@@ -1,14 +1,18 @@
 package adt.inventario.dao;
 
 import adt.inventario.model.Product;
+import org.hibernate.HibernateException;
 
 import java.util.List;
 
 public interface ProductDAO {
+    void addProduct(Product product) throws HibernateException;
+    void updateProduct(Product product);
     List<Product> list();
-    void use();
-    void hasProduct(String productName);
-    Product getProduct();
+    void use(int number);
+    void removeProduct();
+    List<Product> hasProduct(String productName);
+
     void exit();
 
 }
