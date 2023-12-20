9cf583dcdad6af2ba62da70c43100ebf4a1e17af
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 20:23:08 +0100

    Add interfaceś functions

--- src/main/java/adt/inventario/dao/ProductDAO.java
+++ src/main/java/adt/inventario/dao/ProductDAO.java
@@ -1,4 +1,14 @@
 package adt.inventario.dao;
 
+import adt.inventario.model.Product;
+
+import java.util.List;
+
 public interface ProductDAO {
+    List<Product> list();
+    void use();
+    void hasProduct(String productName);
+    Product getProduct();
+    void exit();
+
 }
