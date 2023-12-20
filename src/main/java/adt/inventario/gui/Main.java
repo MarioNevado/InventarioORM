bda2137c87cd562b2693d209017449c95daf7509
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 21:05:50 +0100

    Update main class

--- src/main/java/adt/inventario/gui/Main.java
+++ src/main/java/adt/inventario/gui/Main.java
@@ -1,6 +1,11 @@
 package adt.inventario.gui;
 
 
+import adt.inventario.utils.CSVReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 /*
@@ -15,24 +20,34 @@ Es importante destacar que en caso de que se repita un elemento, no se tiene que
 7;rollo papel higiénico <==== se realizará un update
 Al final se tendrá en la tabla que hay 8 unidades de papel higiénico.
 Una vez realizada la carga de los datos se deberá acceder a ellos a través de palabras reservadas:
-    listar: mostrará todos los suministros que nos quedan.
-    usar x suministro: actualizará la base de datos reduciendo en x el suministo pasado por parámetro. En caso de que la cantidad de suministros
+    - listar: mostrará todos los suministros que nos quedan.
+    - usar x suministro: actualizará la base de datos reduciendo en x el suministo pasado por parámetro. En caso de que la cantidad de suministros
     almacenados sea igual a x se deberá eliminar el suministro del inventario. En caso de que x sea mayor al número de suministros se deberá
     notificar como un error y no realizar ninguna acción.
-    hay suministro: mostrar cuantos suministros nos quedan que contengan la cadena de texto pasada por parámetro.
-    adquirir x suministro: insetará o actualizará el suministro con o en x unidades. Igual que cuando lo lee del fichero.
-    salir: finaliza el programa.
+    - hay suministro: mostrar cuantos suministros nos quedan que contengan la cadena de texto pasada por parámetro.
+    - adquirir x suministro: si lo tiene aumenta en una las unidades
+    - salir: finaliza el programa.
 
 Todos el programa ignorará mayúsculas y minúsculas. Se deberá permitir la ejecución ilimitada del programa solo parando al escribir la palabra salir
 o parando el proceso de ejecución.
  */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String command;
+        CSVReader csv;
         do {
-            System.out.println("->");
+            System.out.print("-> ");
             command = sc.nextLine();
-        }while(command.equalsIgnoreCase("salir"));
+            if(!command.equalsIgnoreCase("salir")){
+                try {
+                    csv = new CSVReader(new File("files/compra.csv"));
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }while(!command.equalsIgnoreCase("salir"));
     }
 }
