10a2c5f64fc3619efc41fc379fc1a4df07a6774c
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 20:39:36 +0100

    Init exception if used units exceed available units

---
+++ src/main/java/adt/inventario/exceptions/UsedUnitsExceedException.java
@@ -0,0 +1,7 @@
+package adt.inventario.exceptions;
+
+public class UsedUnitsExceedException extends Exception {
+    public UsedUnitsExceedException(String message) {
+        super(message);
+    }
+}
