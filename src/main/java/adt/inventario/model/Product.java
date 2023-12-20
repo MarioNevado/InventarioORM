6762116d881e64da2e3611b42a8c302aa8863812
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 20:18:12 +0100

    Add attributes & functions

--- src/main/java/adt/inventario/model/Product.java
+++ src/main/java/adt/inventario/model/Product.java
@@ -1,4 +1,38 @@
 package adt.inventario.model;
 
+import jakarta.persistence.*;
+@Entity
+@Table(name="products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="name")
+    private String name;
+    @Column(name="amount")
+    private int amount;
+
+    public Product() {}
+
+    public Product(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
