eec5e5efbc30c362869ad8e7f05e93540b50ff40
MarioNevado <m.nevadomerino@gmail.com>
2023-12-20 21:04:54 +0100

    Add conditions on readLine()

--- src/main/java/adt/inventario/utils/CSVReader.java
+++ src/main/java/adt/inventario/utils/CSVReader.java
@@ -1,10 +1,40 @@
 package adt.inventario.utils;
 
+import adt.inventario.model.Product;
+import adt.inventario.pojo.ProductPojo;
+
+import java.io.*;
+
 public class CSVReader {
-    public CSVReader() {
+    File file;
 
+    public CSVReader(File file) throws IOException, NumberFormatException {
+        if (!file.exists() || !file.isFile()) {
+            throw new FileNotFoundException("El archivo no existe o no es un archivo");
+        } else {
+            this.file = file;
+            readCSV();
+        }
     }
-    public void readCsv(){
 
+    public void readCSV() throws IOException, NumberFormatException {//si ya esta añadido ese producto aumenta la cantidad
+        String line = "";
+        Product product;
+        ProductPojo pojo = new ProductPojo();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while((line = reader.readLine()) != null){
+                line = line.toLowerCase();
+                product = new Product(line.split(";", 2)[1], Integer.parseInt(line.split(";", 2)[0]));
+                if (!pojo.hasProduct(product.getName()).isEmpty()){
+                    pojo.addProduct(product);
+                }else {
+
+                }
+            }
+        } catch (IOException ioex) {
+            throw new IOException("Error leyendo el fichero");
+        }catch(NumberFormatException nf){
+            throw new NumberFormatException("Error intentando obtener la cantidad del producto" + line);
+        }
     }
 }
