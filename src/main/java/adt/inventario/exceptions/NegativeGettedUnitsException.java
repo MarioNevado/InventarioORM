595bb9a9b33ecaf47848ac9307c819f336e0633c
mario <m.nevadomerino@gmail.com>
2023-12-28 19:21:45 +0100

    feature: add NegativeGettedUnitsException class

---
+++ src/main/java/adt/inventario/exceptions/NegativeGettedUnitsException.java
@@ -0,0 +1,7 @@
+package adt.inventario.exceptions;
+
+public class NegativeGettedUnitsException extends Exception{
+    public NegativeGettedUnitsException(String message){
+        super(message);
+    }
+}
