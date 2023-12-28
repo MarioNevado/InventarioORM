77990c83a008a95789031aae36afc7ec5e4101af
mario <m.nevadomerino@gmail.com>
2023-12-28 11:40:33 +0100

    Refactor: Delete unused code

--- src/main/java/adt/inventario/model/Product.java
+++ src/main/java/adt/inventario/model/Product.java
@@ -24,28 +24,18 @@ public class Product {
         this.name = name;
         this.amount = amount;
     }
-
-
     public void setId(int id) {
         this.id = id;
     }
-
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAmount() {
         return amount;
     }
-
     public void setAmount(int amount) {
         this.amount = amount;
     }
